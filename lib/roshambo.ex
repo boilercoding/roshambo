12d362c846769a6d31f5535c2771a60d03af74c6
boilercoding <boilercoding@gmail.com>
2017-07-29 13:07:32 -0400

    Creates flag to tell cpu what move to play

--- lib/roshambo.ex
+++ lib/roshambo.ex
@@ -5,33 +5,41 @@ defmodule Roshambo do
   end
 
   def main(argv) do
-    player_move = List.first(argv)
-    play(player_move)
+    argv
+    |> parse_args
+    |> determine_winner
+    |> IO.puts
   end
 
-  defp play(player_move) do
-    get_cpu_move()
-    |> determine_winner(player_move)
-    |> IO.puts
+  defp parse_args(argv) do
+    switches = [rock: :bolean, paper: :boolean, scissors: :boolean]
+    parse = OptionParser.parse(argv, switches: switches)
+
+    case parse do
+      {[{switch, true}], [player_move], _} ->
+        {to_string(switch), player_move}
+      {_, [player_move], _} ->
+        {get_cpu_move(), player_move}
+    end
   end
 
   defp get_cpu_move do
     Enum.random(["rock", "paper", "scissors"])
   end
 
-  defp determine_winner("paper", "rock") do
+  defp determine_winner({"paper", "rock"}) do
     "You lost, computer played paper"
   end
-  defp determine_winner("rock", "scissors") do
+  defp determine_winner({"rock", "scissors"}) do
     "You lost, computer played rock"
   end
-  defp determine_winner("scissors", "paper") do
+  defp determine_winner({"scissors", "paper"}) do
     "You lost, computer played scissors"
   end
-  defp determine_winner(cpu_move, player_move) when cpu_move == player_move do
+  defp determine_winner({cpu_move, player_move}) when cpu_move == player_move do
     "It was a tie - you both played #{cpu_move}"
   end
-  defp determine_winner(cpu_move, _) do
+  defp determine_winner({cpu_move, _}) do
     "You win! Computer played #{cpu_move}"
   end
 end
